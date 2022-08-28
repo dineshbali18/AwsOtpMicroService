54df077913b61133fd91bc56e86284dd3d25f083
dineshbali18 <80048098+dineshbali18@users.noreply.github.com>
2022-08-28 11:56:48 +0530

    Update contestants.js

--- routes/contestants.js
+++ routes/contestants.js
@@ -1,6 +1,7 @@
 const express=require("express");
 const router=express.Router();
-const {getAllContestants}=require("../controllers/contestants")
+const {getAllContestants,createContestant}=require("../controllers/contestants")
 
-router.get("/all/contestants",getAllContestants)
+router.get("/bali/all/contestants",getAllContestants)
+router.post("/bali/contestant/create",createContestant)
 module.exports=router;
